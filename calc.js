4fe644757b0724a6940863efecf474fc376be5df
vdtuinj <73612884+vdtuinj@users.noreply.github.com>
2024-01-10 02:51:11 +0100

    Change Add function

--- calc.js
+++ calc.js
@@ -1,35 +1,70 @@
-console.log("Calculator Script loaded...")
+console.log("Calculator Script loaded...");
+var operator = null;
 
-function press_one(){
-    
-    scherm = document.getElementById("scherm");
-    scherm.value = scherm.value + "1";
-    return;
-}
+/* 
 
-function press_c(){
+Working functions:
+- Clear
+- Add number
+
+*/
+
+function press_c(){ //Clear Calculator
     
     scherm = document.getElementById("scherm");
     scherm.value = "";
     return;
 }
-function press_add(){
-    
-    vorige = parseFloat(document.getElementById("previousval").innerText);
-    inputval = parseFloat(document.getElementById("scherm").value);
-    if(inputval = !typeof(Number)){
-        inputval = parseFloat(0);
-    }
-    resultaat = vorige + inputval;
-    console.log(resultaat);
-    alert(resultaat);
-    scherm.value = "";
-    return;
-}
-
-function press_num(nummer){
+function press_num(nummer){ //Add number to display
     
     scherm = document.getElementById("scherm");
     scherm.value = scherm.value + nummer;
     return;
+}
+
+// Functions to be improved:
+/* Legacy add function
+function press_add(){
+    
+    geheugen = document.getElementById("previousval");
+    invoer = document.getElementById("scherm");
+
+    x = parseFloat(geheugen.innerHTML);
+    y = parseFloat(invoer.value);
+
+    if(isNaN(y)){
+        return y = 0;
+    }
+    resultaat = x + y;
+    console.log(resultaat);
+    
+    geheugen.innerHTML = resultaat;
+    invoer.value = resultaat;
+    return;
+}
+
+*/
+
+function press_operator(operator){
+switch (operator) {
+    case "+":
+        //Do Something
+        add();
+        
+        break;
+
+    default:
+        break;
+}
+    return;
+}
+
+function add(){
+    if (operator == null){
+        alert(' no operator')
+        operator = '+'
+    }
+else{
+    alert(operator)
+}
 }
